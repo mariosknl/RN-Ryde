0817938415f54a376df90d6b2c0bdd4cbd94d202
mariosknl <mariosknl@outlook.com>
2024-08-18 12:10:20 +0300

    confirm ride screen

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,10 +1,34 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import RideLayout from "@/app/components/RideLayout";
+import DriverCard from "@/app/components/DriverCard";
+import CustomButton from "@/app/components/CustomButton";
+import { router } from "expo-router";
+import { useDriverStore } from "@/store";
 
 const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+
   return (
-    <View>
-      <Text>Confirm Ride</Text>
-    </View>
+    <RideLayout title="Choose a Driver" snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        renderItem={({ item }) => (
+          <DriverCard
+            item={item}
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id)}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className={"mx-5 mt-10"}>
+            <CustomButton
+              title="Select Ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
   );
 };
 
