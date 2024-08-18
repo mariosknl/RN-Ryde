b3828e0008cd828db67a1288c0c1aa5609c46f27
mariosknl <mariosknl@outlook.com>
2024-08-18 09:42:51 +0300

    NeonDB implementation & API routes

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,34 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function POST(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+
+    const { name, email, clerkId } = await request.json();
+
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+    }
+
+    const response = await sql`
+  INSERT INTO users (
+    name,
+    email,
+    clerkId    
+  )
+  VALUES (
+    ${name},
+    ${email},
+    ${clerkId}    
+  )
+`;
+
+    return new Response(JSON.stringify({ data: response, status: 201 }));
+  } catch (err) {
+    console.error(err);
+    return Response.json({ error: err, status: 500 });
+  }
+}
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import { Link, useRouter } from "expo-router";
 import OAuth from "@/app/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -58,7 +59,14 @@ const SignUp = () => {
       });
 
       if (completeSignUp.status === "complete") {
-        // TODO: Create a db user
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
           ...verification,
