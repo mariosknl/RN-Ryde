2f3f89cb1ed7a9e2f09f4538ecd30cf6e209bcf8
mariosknl <mariosknl@outlook.com>
2024-08-19 10:54:00 +0300

    implementing oAuth Authentication

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,9 +1,24 @@
-import { Image, Text, View } from "react-native";
+import { useOAuth } from "@clerk/clerk-expo";
+import { router } from "expo-router";
+import { Alert, Image, Text, View } from "react-native";
+
 import CustomButton from "@/components/CustomButton";
 import { icons } from "@/app/constants";
+import { googleOAuth } from "@/lib/auth";
 
 const OAuth = () => {
-  const handleGoogleSignIn = async () => {};
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleSignIn = async () => {
+    const result = await googleOAuth(startOAuthFlow);
+
+    if (result.code === "session_exists") {
+      Alert.alert("Success", "Session exists. Redirecting to home screen.");
+      router.replace("/(root)/(tabs)/home");
+    }
+
+    Alert.alert(result.success ? "Success" : "Error", result.message);
+  };
 
   return (
     <View>
@@ -14,7 +29,7 @@ const OAuth = () => {
       </View>
 
       <CustomButton
-        title="Log in with Google"
+        title="Log In with Google"
         className="mt-5 w-full shadow-none"
         IconLeft={() => (
           <Image
